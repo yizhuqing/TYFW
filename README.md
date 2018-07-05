316d4c98777ec8851e1e124b7f6ffd679662196b
yizhuqing@qq.com <yizhuqing@qq.com>
2018-07-05 13:56:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# TYFW
+基于wepy框架的“天演防伪”小程序
+
+## 需要预先安装[wepy](https://github.com/Tencent/wepy)
+```npm install wepy-cli -g```
+
+## 启动
+```wepy build --watch```
---
+++ src/util/Http.js
@@ -0,0 +1,78 @@
+import wepy from 'wepy'
+import Tips from './Tips';
+const app = getApp();
+
+// HTTP工具类
+export default class http {
+  static async request (method, url, data) {
+    const param = {
+      url: url,
+      method: method,
+      data: data
+    };
+    Tips.loading();
+    const res = await wepy.request(param);
+    if (this.isSuccess(res)) {
+      return res.data;
+    } else {
+      console.error(method, url, data, res);
+      throw this.requestException(res);
+    }
+  }
+
+  /**
+   * 判断请求是否成功
+   */
+  static isSuccess(res) {
+    const wxCode = res.statusCode;
+    // 微信请求错误
+    if (wxCode !== 200) {
+      return false;
+    }
+    const ar = res.data;
+    // Session是否超期
+    if (ar.Code == 401) {
+      wepy.navigateTo({
+        url:'../home/index',
+      })
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * 异常
+   */
+  static requestException(res) {
+    const error = {};
+    error.statusCode = res.statusCode;
+    const wxData = res.data;
+    const serverData = wxData.data;
+    if (serverData) {
+      error.serverCode = wxData.code;
+      error.message = serverData.message;
+      error.serverData = serverData;
+    }
+    return error;
+  }
+
+  static get (url, data) {
+    return this.request('GET', url, data)
+  }
+
+  static put (url, data) {
+    return this.request('PUT', url, data)
+  }
+
+  static post (url, data) {
+    return this.request('POST', url, data)
+  }
+
+  static patch (url, data) {
+    return this.request('PATCH', url, data)
+  }
+
+  static delete (url, data) {
+    return this.request('DELETE', url, data)
+  }
+}
